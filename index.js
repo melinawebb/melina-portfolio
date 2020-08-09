3ec5fdaf5031261dd616949bc390563de4c4543a
melinawebb <64170051+melinawebb@users.noreply.github.com>
2020-08-08 21:09:49 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
     $('div.hidden').fadeIn(1500).removeClass('hidden');
 });
+
+jQuery(function($){
+    var headerAnchors = $('#screen > header a').click(function(){
+      headerAnchors.removeClass('active');
+      $(this).addClass('active');
+    });
+  });
