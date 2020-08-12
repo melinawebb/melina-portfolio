8335393818f3a560016ed960e4ae1caaf4446df6
melinawebb <64170051+melinawebb@users.noreply.github.com>
2020-08-11 18:42:29 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-$(document).ready(function () {
-    $('div.hidden').fadeIn(1500).removeClass('hidden');
-});
+var homeElement = document.getElementById("home");
+var workElement = document.getElementById("work");
+var aboutElement = document.getElementById("about");
+var contactElement = document.getElementById("contact");
 
-jQuery(function($){
-    var headerAnchors = $('#screen > header a').click(function(){
-      headerAnchors.removeClass('active');
-      $(this).addClass('active');
-    });
-  });
+var navItem = document.getElementsByClassName("nav-item");
+
+function decide() {
+    var contactElementHeight = contactElement.clientHeight / 2;
+    if(contactElement.getBoundingClientRect().top <= window.innerHeight - contactElementHeight) {
+        navItem[3].classList.add("clicked");
+        navItem[0].classList.remove("clicked");
+        navItem[1].classList.remove("clicked");
+        navItem[2].classList.remove("clicked");
+    } else if(aboutElement.getBoundingClientRect().top <= 0) {
+        navItem[2].classList.add("clicked");
+        navItem[0].classList.remove("clicked");
+        navItem[1].classList.remove("clicked");
+        navItem[3].classList.remove("clicked");
+    } else if(workElement.getBoundingClientRect().top <= 0) {
+        navItem[1].classList.add("clicked");
+        navItem[0].classList.remove("clicked");
+        navItem[2].classList.remove("clicked");
+        navItem[3].classList.remove("clicked");
+    } else if(homeElement.getBoundingClientRect().top <= 0) {
+        navItem[0].classList.add("clicked");
+        navItem[1].classList.remove("clicked");
+        navItem[2].classList.remove("clicked");
+        navItem[3].classList.remove("clicked");
+    }
+}
+
+
+document.onscroll = decide;
